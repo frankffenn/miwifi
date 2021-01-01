2a307524fdab5599a19a9b36999244bd64b207fc
frank <www.417329062@qq.com>
2021-01-01 22:30:39 +0800

    update auth loop

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"log"
+	"time"
 )
 
 func main() {
 	m := NewMiWifi()
+	resp, _ := m.Call()
+	if resp.Code < 0 {
+		log.Println("auth failed:", resp.Message)
+		return
+	}
+
+	log.Println("connect wifi success, check your internet!")
+
 	ips, err := fetchIP()
 	if err != nil {
 		log.Println("fetch ip failed:%v", err)
@@ -17,15 +26,15 @@ func main() {
 		m.SetProxy(ip)
 
 		for i := 0; i < 4; i++ {
+			time.Sleep(100 * time.Second)
+
 			resp, _ := m.Call()
 			if resp.Code < 0 {
 				log.Println("auth failed:", resp.Message)
 				return
 			}
 
-			log.Println("connect wifi success, check your internet!")
+			log.Printf("keep alive ..., ip:%s, retry times:%d\n", ip, i+1)
 		}
 	}
-
-	m.KeepAlive()
 }
